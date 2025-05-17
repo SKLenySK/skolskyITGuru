575f462651cbb01bd672ab249f40508f8ff1b59a
SKLenySK <michal@lenhart.eu>
2025-05-17 20:56:57 +0200

    aktualizácia

--- AppDeployment/ListUninstallCommand.ps1
+++ AppDeployment/ListUninstallCommand.ps1
@@ -1,4 +1,23 @@
-﻿# Define the path for 64-bit and 32-bit applications in the registry
+﻿<#
+.SYNOPSIS
+Získanie zoznamu odinštalačných príkazov pre aplikácie v systéme Windows (64-bit aj 32-bit).
+
+.DESCRIPTION
+Tento skript prehľadáva registre systému Windows, aby získal informácie o všetkých nainštalovaných aplikáciách. 
+Zohľadňuje obe vetvy registra – pre 64-bitové aj 32-bitové aplikácie – a zhromažďuje údaje ako názov aplikácie, 
+vydavateľ, dátum inštalácie, príkaz na odinštalovanie a umiestnenie inštalácie.
+
+Výsledky sa zobrazia v tabuľke v konzole, prípadne je možné ich exportovať do CSV súboru.
+
+.VYUŽITIE
+- zistenie odinštalátora pre Deployment Intune Aplikácií
+
+.COPYRIGHT
+© 2025 skolskyIT.guru. Všetky práva vyhradené.
+#>
+
+
+# Define the path for 64-bit and 32-bit applications in the registry
 $regPaths = @(
     "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
     "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
